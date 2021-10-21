2cec62322e6dfe991d980a94cd9a647ad8835366
Konner Hartman <konner.hartman@yahoo.com>
2021-10-21 15:36:58 -0500

    resolved two bugs.

--- server.js
+++ server.js
@@ -26,18 +26,17 @@ const path = require('path');
 const PORT = 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Import our modular routers for /tips and /feedback
 const htmlRouter = require('./routes/html');
 const apiRouter = require('./routes/api');
 
-app.use('/html', htmlRouter);
+app.use('/', htmlRouter);
 app.use('/api', apiRouter);
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 app.listen(PORT, () =>
